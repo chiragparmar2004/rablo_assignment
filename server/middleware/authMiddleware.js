d3bebfc5515d1e3c80716031b75bc0b148689bd4
chiragparmar2004 <chiragparmar4780@gmail.com>
2024-07-07 09:36:51 +0530

    modified middleware logic

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,24 +1,7 @@
 import jwt from "jsonwebtoken";
-
-const authMiddleware = (req, res, next) => {
-  const token = req.header("Authorization")?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Access denied" });
-
-  try {
-    const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified;
-    next();
-  } catch (error) {
-    res.status(400).json({ message: "Invalid token" });
-  }
-};
-
-module.exports = authMiddleware;
-
-import jwt from "jsonwebtoken";
 import sendResponse from "../lib/responseHelper.js";
 
-export const verifyToken = (req, res, next) => {
+export const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) return sendResponse(res, 401, "Not Authenticated");
